664ca2c34288b71b2fed98dd37013adcfd9789dc
shreyarathore01 <146115860+shreyarathore01@users.noreply.github.com>
2023-10-18 19:11:03 +0530

    Add files via upload

---
+++ FyersAPIHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FyersAPI;
+namespace oderapp
+{
+    class FyersAPIHelper
+    {
+        public string PlaceOrder(Order order)
+        {
+            // TODO: Implement the code to place the order using the Fyers API.
+            return "Order placed successfully.";
+        }
+    }
+    public enum Side
+    {
+        Buy,
+        Sell
+    }
+
+    public class Order
+    {
+        public string Symbol { get; set; }
+        public Side Side { get; set; }
+        public int Qty { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FyersAPI;
+
+namespace oderapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FyersAPIHelper fyersAPIHelper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fyersAPIHelper = new FyersAPIHelper();
+        }
+        private void PlaceOrder_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a new order object.
+            Order order = new Order();
+
+            // Populate the order object with the user's input.
+            order.Symbol = symbolComboBox.SelectedItem.ToString();
+            order.Side = (Side)Enum.Parse(typeof(Side), sideComboBox.SelectedIndex.ToString());
+            order.Qty = Convert.ToInt32(quantityTextBox.Text);
+
+            // Place the order using the Fyers API.
+            string response = fyersAPIHelper.PlaceOrder(order);
+
+            // Display the response in the logs tab.
+            textBoxLogs.Text += $"Request: {order}\nResponse: {response}\n";
+        }
+    }
+}
